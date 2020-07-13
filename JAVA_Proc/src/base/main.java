c71b83e57a13aa9ce8ec3788a3a624ccb4238f1a
young324 <duddms243@naver.com>
2020-07-13 15:08:43 +0900

    commit test

--- JAVA_Proc/src/base/main.java
+++ JAVA_Proc/src/base/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("push test2");
+		System.out.println("오류 수정 후 테스트");
 	}
 
 }
