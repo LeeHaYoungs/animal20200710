eec4bf579357ca7e1bd789a86a500ce54c1f53f5
Huchu0623 <65884625+Huchu0623@users.noreply.github.com>
2020-07-15 15:20:01 +0900

    20200715 진료예약확인

--- JAVA_Proc/src/base/Appoint1.java
+++ JAVA_Proc/src/base/Appoint1.java
@@ -23,73 +23,61 @@ public class Appoint1 {
 
 	public void createFrame() {
 
-      JFrame f = new JFrame("진료예약 확인");   
-      
-      f.setBounds(200, 200, 500, 630);
-      f.setLayout(null);
-      
-    
-      
-      Font font = new Font("맑은고딕",Font.BOLD,30);
-      Font font2 = new Font("맑은고딕",Font.BOLD,25);
-      Font btnFont = new Font("맑은고딕",Font.BOLD,30);
-      
-      JLabel a1 = new JLabel();
-      a1.setText("진료예약 확인");
-      
-      a1.setBounds(200,20,200,100);
-      a1.setLocation(150,10);
-      
-      a1.setFont(font);
-      
-      
-      
+		Frame f=new Frame("진료예약 확인");
+  		f.setBounds(0,100,400,600);
+          f.setBackground(Color.lightGray);
+          f.setLayout(new BorderLayout());
+          
+          Font fontTitle=new Font(Font.SANS_SERIF,Font.BOLD,24);
+          Font fontCon=new Font("맑은 고딕",Font.PLAIN,16);
+          
+          Panel pCon = new Panel();
+          pCon.setLayout(null);
+          pCon.setSize(400,600);
+          
+          Panel pTitle=new Panel();
+          pTitle.setSize(400, 50);
+          Label title=new Label("진료예약확인");
+          pTitle.setBackground(Color.pink);
+          pTitle.setFont(fontTitle);
+          pTitle.setLocation(0, 30);
+          pTitle.add(title);
+          f.add(pTitle);
+          
+          Panel pConL = new Panel();
+          pConL.setSize(120, 400);
+          pConL.setLocation(50, 90);
+          Label name=new Label("보호자명 : ",Label.RIGHT);
+          pConL.add(name);
+          Label phone=new Label("보호자 연락처 : ",Label.RIGHT);
+          pConL.add(phone);
+          pCon.add(pConL);
+     
+          Panel pConR = new Panel();
+          pConR.setSize(190, 400);
+          pConR.setLocation(150, 90);
    
+          TextField animalnameA=new TextField(18);
+          pConR.add(animalnameA);
+          TextField animalnameB=new TextField(18);
+          pConR.add(animalnameB);
+          
+          pCon.add(pConR);
+         
+          Button btn1 = new Button("메인화면");
+          Button btn2 = new Button("확인");
+          btn1.setSize(200, 50);
+          btn1.setLocation(0,515);
+          btn2.setSize(200, 50);
+          btn2.setLocation(201,515);
+          pCon.add(btn1);
+          pCon.add(btn2);
+          
+          f.add(pCon);
+     
       
-
-      JLabel a2 = new JLabel();
-      a2.setText("보호자명");
-      a2.setBounds(200,200,400,200);
-      a2.setLocation(20,50);
-      a2.setFont(font2);
-      
-      JTextField tf1 = new JTextField(20);
-      tf1.setBounds(200,100,150,30);
-      tf1.setLocation(140, 135);
-      
-      JLabel a3 = new JLabel();
-      a3.setText("보호자 연락처");
-      a3.setBounds(500,600,200,100);
-      a3.setLocation(20,200);
-      a3.setFont(font2);
-      
-      JTextField tf2 = new JTextField(20);
-      tf2.setBounds(200,100,150,30);
-      tf2.setLocation(200, 235);
-      
-      
-      
-      Button btn1 = new Button("메인화면");
-      Button btn2 = new Button("확인");
-      btn1.setSize(200, 50);
-      btn1.setLocation(10,540);
-      btn2.setSize(200, 50);
-      btn2.setLocation(290,540);
-      
-      btn1.setFont(btnFont);
-      btn2.setFont(btnFont);
-      
-      
-      
-      f.add(a1);
-      f.add(a2);
-      f.add(a3);
-      
-      f.add(tf1);
-      f.add(tf2);
-      
-      f.add(btn1);
-      f.add(btn2);
+     
+      f.setVisible(true);
       
       
       btn1.addActionListener(new ActionListener() {
@@ -113,7 +101,7 @@ public class Appoint1 {
 			}
 	    	
 	    });
-      f.setVisible(true);
+    
       f.addWindowListener(new WindowAdapter() {
     	  
     	  
